c52db945c5d3c291c8ac330aec2089c8eadcd0dd
NIHARIKA983 <niharikagowda1996@gmail.com>
2021-07-02 16:47:26 +0530

    Added:To Initailize Position of the Player

---
+++ src/main/java/com/bridgelabz/SnakeAndLadderProblem.java
@@ -0,0 +1,10 @@
+package com.bridgelabz;
+
+public class SnakeAndLadderProblem {
+    //variables
+    int position=0;
+    public static void main(String[] args){
+        System.out.println("Welcome To Snake And Ladder Game");
+    }
+
+}
